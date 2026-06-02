bcc1fd98638789a26715c68053be483465825033
Aaquib Syed <syed-aaquib.mn@capgemini.com>
2026-06-02 10:58:06 +0530

    add port from env

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import aboutRoutes from "../src/routes/about.routes.ts";
 import skillsRoutes from "../src/routes/skills.route.ts";
 import contactRoutes from "../src/routes/contact.routes.ts";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
